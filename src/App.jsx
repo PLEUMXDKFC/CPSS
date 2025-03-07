286a5bd2315f5d960b5ecfec9ccda9266bbf9429
PLEUMXDKFC <kidtapad.gm@gmail.com>
2025-03-07 15:56:01 +0700

    FirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Test from './Page/test'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Test />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/Page/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function test() {
+  return (
+    <div className='text-center justify-center'>
+        <h1 className='text-red-500'>Hello World !</h1>
+        <button className='w-50 bg-blue-500 text-white rounded-lg'>Test</button>
+    </div>
+  )
+}
+
+export default test
