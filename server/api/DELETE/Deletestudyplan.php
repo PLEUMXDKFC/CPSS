cf471d1218b92a39d0001dc26170e65f31ea424c
PLEUMXDKFC <kidtapad.gm@gmail.com>
2025-10-10 17:06:12 +0700

    bruh

--- server/api/DELETE/Deletestudyplan.php
+++ server/api/DELETE/Deletestudyplan.php
@@ -1,39 +1,17 @@
 <?php
-require("../conn.php");
-header("Content-Type: application/json; charset=UTF-8");
+    require("../conn.php");
+    header("Content-Type: application/json; charset=UTF-8");
 
+        $data = json_decode(file_get_contents('php://input'), true);
+        $planid = $data['planid'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['_method']) && $_POST['_method'] === 'DELETE') {
-    $planid = $_POST['planid'];
+        try {
+            $stmt = $conn->prepare("DELETE FROM study_plans WHERE planid = :planid");
+            $stmt->bindParam(":planid", $planid);
+            $stmt->execute();
 
-    try {
-        $conn->beginTransaction();
-
-        $stmt = $conn->prepare("DELETE FROM course_information WHERE subject_id IN (SELECT subject_id FROM subject WHERE planid = :planid)");
-        $stmt->bindParam(":planid", $planid);
-        $stmt->execute();
-
-        $stmt = $conn->prepare("DELETE FROM subject WHERE planid = :planid");
-        $stmt->bindParam(":planid", $planid);
-        $stmt->execute();
-
-        $stmt = $conn->prepare("DELETE FROM group_information WHERE planid = :planid");
-        $stmt->bindParam(":planid", $planid);
-        $stmt->execute();
-
-        $stmt = $conn->prepare("DELETE FROM study_plans WHERE planid = :planid");
-        $stmt->bindParam(":planid", $planid);
-        $stmt->execute();
-
-        $conn->commit();
-
-        echo json_encode(["status" => "success", "message" => "Data deleted successfully"]);
-    } catch (Exception $e) {
-        $conn->rollBack();
-        echo json_encode(["status" => "error", "message" => $e->getMessage()]);
-    }
-} else {
-    echo json_encode(["status" => "error", "message" => "Invalid request method"]);
-}
-
+            echo json_encode(value: ["status" => "success", "message" => "Data deleted successfully"]);
+        } catch (Exception $e) {
+            echo json_encode(["status" => "error", "message" => $e->getMessage()]);
+        }
 ?>
